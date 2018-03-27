c5e78eb976369d4f31d6e385cd63b14ace5a1146
Vasyl <admin@gmail.com>
2018-03-27 20:23:52 +0300

    init

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 	include_once 'includes/header.php';
 	include_once 'includes/content.php';
 	include_once 'includes/footer.php';
+	include_once 'css/header.css';
+	include_once 'css/content.css';
+	include_once 'css/footer.css';
+	include_once 'css/main.css';
 	?>
     </head>
     <body>
