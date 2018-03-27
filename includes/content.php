fb7649fc0a0127748f1bf47b59ed09c7123349fd
yura <yura@com.ua>
2018-03-27 20:26:05 +0300

    add h2 in content.php

--- includes/content.php
+++ includes/content.php
@@ -1,8 +1,10 @@
 <div id="main">
     <p>
+    <h2>Lorem ipsum dolor</h2>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius, tortor et posuere egestas, lorem odio mollis dolor, in lacinia enim justo sed nulla. Nam a porta arcu. Suspendisse posuere pulvinar mauris at hendrerit. Donec vel malesuada est. Mauris quis nunc orci. Phasellus ultricies risus sit amet tincidunt iaculis. Quisque quis fringilla dui, nec posuere turpis. Mauris fermentum urna id enim sollicitudin luctus. Proin ultrices turpis ante, placerat sodales enim egestas ultricies. Quisque efficitur scelerisque sodales.
     </p>
     <p>
+        <h2>Cras pulvinar elit</h2>
 Cras pulvinar elit sed nibh interdum, hendrerit elementum ligula viverra. Cras vitae ultrices felis, non blandit ex. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce posuere lobortis risus, sed viverra nisl lobortis laoreet. Proin eleifend ipsum porttitor ex congue imperdiet. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis condimentum non justo pulvinar mollis. In sed erat eros. Duis lectus urna, consequat in libero non, aliquam convallis nibh.
     </p>
 </div>
