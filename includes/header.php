fb18dd18bcd13d3f3d63acdbaf32db164c7f70dc
yura <yura@com.ua>
2018-03-27 20:03:52 +0300

    add to header.php

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,11 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/main.css" rel="stylesheet" type="text/css"/>
+        <link href="../css/main.css" rel="stylesheet" type="text/css"/>
+        <title>BST SITE</title>
+    </head>
+    <body>
+        <div></div>
 
