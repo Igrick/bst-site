6a9526e60dca05d8529cd4644e0446d4bf7da55d
Vasyl <admin@gmail.com>
2018-03-27 20:12:00 +0300

    init

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
         <meta charset="UTF-8">
 	<link href="css/main.css" rel="stylesheet" type="text/css"/>
         <title></title>
+	<?php
+	include_once 'includes/header.php';
+	include_once 'includes/content.php';
+	include_once 'includes/footer.php';
+	?>
     </head>
     <body>
 	<?php
